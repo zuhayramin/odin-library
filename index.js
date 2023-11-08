307c7a9a819e37a9bd3f5937c4b3c356f80eaebd
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-11-08 16:00:43 +0500

    Prevent default on submit button obtain form info

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const form = document.querySelector("form")
 
+// Prevent form submission on button click
+document
+    .getElementById("submit-btn")
+    .addEventListener("click", function (event) {
+        event.preventDefault()
+        console.log(form.title.value)
+    })
+
+const myLibrary = []
+
+function Book() {
+    // the constructor...
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
