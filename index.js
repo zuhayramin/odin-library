6d132f3aa4a4d47f1292ba5988c936dd12b50609
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-11-11 16:54:27 +0500

    Add function to render all books on screen

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form")
+const booksContainer = document.querySelector(".display-books")
 let myLibrary = []
 
 const storedData = localStorage.getItem("myLibrary")
@@ -8,6 +9,10 @@ if (storedData !== null) {
 } else {
     console.log("No data found for the specified key.")
 }
+
+myLibrary.forEach((book) => {
+    addBookToLibrary(book)
+})
 
 // Prevent form submission on button click
 document
@@ -22,14 +27,14 @@ document
             form.read.checked
         )
         myLibrary.push(book)
-        console.log(myLibrary)
-        console.log(JSON.stringify(myLibrary))
         try {
             localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
             console.log("Data has been stored successfully.")
         } catch (error) {
             console.error("Error while storing data in localStorage:", error)
         }
+
+        addBookToLibrary(book)
     })
 
 function Book(title, author, pages, read) {
@@ -39,6 +44,30 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(book) {
+    const newBook = document.createElement("div")
+    newBook.classList.add("book")
+
+    const bookTitle = document.createElement("div")
+    bookTitle.classList.add("book-title")
+    bookTitle.innerText = book.title
+
+    const bookAuthor = document.createElement("div")
+    bookAuthor.classList.add("book-author")
+    bookAuthor.innerText = book.author
+
+    const bookPages = document.createElement("div")
+    bookPages.classList.add("book-pages")
+    bookPages.innerText = book.pages
+
+    const bookRead = document.createElement("div")
+    bookRead.classList.add("book-read")
+    bookRead.innerText = book.read
+
+    newBook.appendChild(bookTitle)
+    newBook.appendChild(bookAuthor)
+    newBook.appendChild(bookPages)
+    newBook.appendChild(bookRead)
+
+    booksContainer.appendChild(newBook)
 }
